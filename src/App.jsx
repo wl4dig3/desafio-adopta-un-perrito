ba1640303b40fee2d6afc6b367da69750ae0b44d
wl4dig3 <wladige@gmail.com>
2024-02-28 23:26:46 -0300

    feat: se agrega info a utilizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import './App.css';
 import Cards from './components/Cards';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import cardStr from './constans/cardStr';
 
 
 function App() {
@@ -10,8 +11,11 @@ function App() {
   return (
     <>
       <Header headerTitle='Adopta una mascota' />
-      <Cards />
-      <Footer />
+      <Cards img={cardStr.bartolo.img} title={cardStr.bartolo.titulo} description={cardStr.bartolo.description} btntitle={cardStr.bartolo.btntitle} />
+      <Cards img={cardStr.messi.img} title={cardStr.messi.titulo} description={cardStr.messi.description} btntitle={cardStr.messi.btntitle} />
+      <Cards img={cardStr.gohan.img} title={cardStr.gohan.titulo} description={cardStr.gohan.description} btntitle={cardStr.gohan.btntitle} />
+      <Cards img={cardStr.princesa.img} title={cardStr.princesa.titulo} description={cardStr.princesa.description} btntitle={cardStr.princesa.btntitle} />
+      <Footer footerTitle={cardStr.FOOTER} />
     </>
   )
 }
---
+++ src/constans/cardStr.js
@@ -0,0 +1,30 @@
+const cardStr = {
+    FOOTER: 'Explora nuestra galeria de adopción de perros para encontrar a tu compañero perfecto. Tenemos una variedad de perros con diferentes personalidades y tamaños, todos en busca de un hogar amoroso. Cada imagen capturasu esencia única. Adopta un perro y bríndale una segunda oportunidad. Encuentra a tu compañero peludo ara siempre y toda la vida.',
+    bartolo: {
+  img: "http://placekitten.com/g/200/300",
+  titulo: "Bartolo",
+  description: "Lleno de energia y listo para jugar. ¡Dale a Bartolo el hogar amoroso que se merece!",
+  btntitle: "Husky",
+},
+messi: {
+    img: "http://placekitten.com/g/200/300",
+    titulo: "Messi",
+    description: "Es juguetón, amigable y se lleva bien con niños y otros animales. ¡Haz de Messi parte de tu familia hoy mismo!",
+    btntitle: "Bobtall",
+  },
+  gohan: {
+    img: "http://placekitten.com/g/200/300",
+    titulo: "Gohan",
+    description: "Un perro de tamaño mediano con un corazón gigante: Hazte amigo de Gohan y experimenta un amor incondicional!.",
+    btntitle: "Sharpei",
+  },
+  princesa: {
+    img: "http://placekitten.com/g/200/300",
+    titulo: "Princesa",
+    description: "Es una compañera leal y cariñosa que adora los mimos y los abrazos. ¡Ayuda a Princesa a encontrar su final feliz!",
+    btntitle: "Beagle",
+  },
+
+};
+
+export default cardStr;
